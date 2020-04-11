379e1580f706d14181484f12cffb99de70960b3a
Kirat Virmani <kirat19984@gmail.com>
2020-04-11 06:08:02 +0530

    Base and saw added. Not drawing

--- TrexRunAI/main.py
+++ TrexRunAI/main.py
@@ -3,28 +3,29 @@ import neat
 import random
 import os
 
-
-WIN_WIDTH = 1000
+WIN_WIDTH = 1920
 WIN_HEIGHT = 800
 FLOOR = 730
 
 GAME_SPEED = 5
 
-
 WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
 BG_IMG = (pygame.image.load(os.path.join("imgs", "BG.png")))
-RUN_IMG = [pygame.transform.scale(pygame.image.load(os.path.join("imgs", "Run__001.png")), (120, 120)), pygame.transform.scale(pygame.image.load(os.path.join("imgs", "Run__004.png")), (120, 120)), pygame.transform.scale(pygame.image.load(os.path.join("imgs", "Run__008.png")), (120, 120))]
+RUN_IMG = [pygame.transform.scale(pygame.image.load(os.path.join("imgs", "Run__001.png")), (120, 120)),
+           pygame.transform.scale(pygame.image.load(os.path.join("imgs", "Run__004.png")), (120, 120)),
+           pygame.transform.scale(pygame.image.load(os.path.join("imgs", "Run__008.png")), (120, 120))]
 SLIDE_IMG = pygame.transform.scale(pygame.image.load(os.path.join("imgs", "Slide__000.png")), (120, 120))
 DEAD_IMG = pygame.transform.scale(pygame.image.load(os.path.join("imgs", "Dead__009.png")), (120, 120))
 JUMP_IMG = pygame.transform.scale(pygame.image.load(os.path.join("imgs", "Jump__001.png")), (120, 120))
-
-
+SAW_IMG = pygame.transform.scale(pygame.image.load(os.path.join("imgs", "Saw.png")), (120, 120))
+TILE_IMG = pygame.image.load(os.path.join("imgs", "Tilemap.png"))
 
 pygame.font.init()
 STAT_FONT = pygame.font.SysFont("roboto", 30)
 
 gen = 0
+
 
 class Player:
     IMGS = RUN_IMG
@@ -34,7 +35,7 @@ class Player:
         self.x = x
         self.y = y
         self.vel = 0
-        self.height = self. y
+        self.height = self.y
         self.img = self.IMGS[0]
         self.tick_count = 0
         self.img_count = 0
@@ -45,12 +46,12 @@ class Player:
         self.tick_count = 0
 
     def move(self):
-        self.tick_count += 1
+        self.tick_count += 2
         disp = self.vel * self.tick_count + 1.5 * self.tick_count ** 2
 
-        if disp >=16:
+        if disp >= 16:
             disp = 16
-        if disp <=0:
+        if disp <= 0:
             disp -= 2
 
         self.y += disp
@@ -78,27 +79,95 @@ class Player:
         return pygame.mask.from_surface(self.img)
 
 
+class Saw:
+    VEL = GAME_SPEED
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.height = 0
+        self.img = SAW_IMG
+        self.passed = False
+        self.saw_height()
+
+    def saw_height(self):
+        choice = random.choice([FLOOR + 120, FLOOR + 180])
+        self.y = choice
+
+    def move(self):
+        self.x -= self.VEL
+
+    def draw(self, win):
+        win.blit(self.img, (self.x, self.y))
+
+    def collide(self, player):
+        player_mask = player.get_mask()
+
+        saw_mask = pygame.mask.from_surface(self.img)
+        offset = (self.x - player.x, self.y - round(player.y))
+
+        if offset:
+            return True
+        return False
 
 
-def draw_window(win, player):
+class Base:
+    VEL = GAME_SPEED
+    IMG = TILE_IMG
+    WIDTH = 1920
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+
+
+def draw_window(win, players, saws,  base):
     win.blit(BG_IMG, (0, 0))
-    player.draw(win)
+    for player in players:
+        player.draw(win)
+    for saw in saws:
+        saw.draw(win)
     pygame.display.update()
+    base.draw(win)
+
 
 def main():
     player = Player(200, 200)
+    players = [player]
 
+    base = Base(800-128)
+    saw = Saw(100, 100)
+    saws = [saw]
+
+    saw.move()
     run = True
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run= False
-        draw_window(WIN, player)
+                run = False
+        draw_window(WIN, players, saws, base)
+
+        base.move()
+        player.move()
 
     pygame.quit()
     quit()
 
+
 main()
-
-
